cfbeaff1c5e7049cddced7b50416ed61fe42cddc
joshua_cm <bylucygrace@gmail.com>
2026-02-06 20:54:48 +0700

    Add Detail Widget as plugin

--- AppBuilder/platform/plugins/included/index.js
+++ AppBuilder/platform/plugins/included/index.js
@@ -1,7 +1,8 @@
 import viewList from "./view_list/FNAbviewlist.js";
 import viewTab from "./view_tab/FNAbviewtab.js";
+import viewDetail from "./view_detail/FNAbviewdetail.js";
 
-const AllPlugins = [viewTab, viewList];
+const AllPlugins = [viewTab, viewList, viewDetail];
 
 export default {
    load: (AB) => {
