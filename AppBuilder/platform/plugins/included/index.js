d5457d39793c5b850c3a8ef7d93799926a3dae26
Wirachot <wirachot@gmail.com>
2026-02-09 11:50:32 +0700

    migate text plugin

--- AppBuilder/platform/plugins/included/index.js
+++ AppBuilder/platform/plugins/included/index.js
@@ -1,7 +1,8 @@
+import viewText from "./view_text/FNAbviewtext.js";
 import viewList from "./view_list/FNAbviewlist.js";
 import viewTab from "./view_tab/FNAbviewtab.js";
 
-const AllPlugins = [viewTab, viewList];
+const AllPlugins = [viewTab, viewList, viewText];
 
 export default {
    load: (AB) => {
---
+++ AppBuilder/platform/plugins/included/view_text/FNAbviewtextComponent.js
@@ -0,0 +1,78 @@
+export default function FNAbviewtextComponent({
+   /*AB,*/
+   ABViewComponentPlugin,
+}) {
+   return class ABAbviewtextComponent extends ABViewComponentPlugin {
+
+
+   constructor(baseView, idBase, ids) {
+      super(
+         baseView,
+         idBase || `ABViewText_${baseView.id}`,
+         Object.assign(
+            {
+               text: "",
+            },
+            ids
+         )
+      );
+   }
+
+   ui() {
+      const ids = this.ids;
+      const settings = this.settings;
+
+      const _uiText = {
+         id: ids.text,
+         view: "template",
+         minHeight: 10,
+         css: "ab-custom-template",
+         borderless: true,
+      };
+
+      if (settings.height) _uiText.height = settings.height;
+      else _uiText.autoheight = true;
+
+      const _ui = super.ui([_uiText]);
+
+      delete _ui.type;
+
+      return _ui;
+   }
+
+   displayText(value) {
+      const ids = this.ids;
+      const result = this.view.displayText(value, ids.text);
+
+      const $text = $$(ids.text);
+
+      if (!$text) return;
+
+      $text.define("template", result);
+      $text.refresh();
+   }
+
+   onShow() {
+      super.onShow();
+
+      // listen DC events
+      const dataview = this.datacollection;
+      const baseView = this.view;
+
+      if (dataview && baseView.parent.key !== "dataview") {
+         ["changeCursor", "cursorStale"].forEach((key) => {
+            baseView.eventAdd({
+               emitter: dataview,
+               eventName: key,
+               listener: (...p) => this.displayText(...p),
+            });
+         });
+      }
+
+      this.displayText();
+   }
+
+
+   };
+
+}
