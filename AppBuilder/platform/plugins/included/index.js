170469c67de79b477f7016e2b3af8a5d8d577181
Wirachot <wirachot@gmail.com>
2026-02-10 17:31:11 +0700

    Migrate Image View

--- AppBuilder/platform/plugins/included/index.js
+++ AppBuilder/platform/plugins/included/index.js
@@ -1,7 +1,8 @@
+import viewImage from "./view_image/FNAbviewimage.js";
 import viewList from "./view_list/FNAbviewlist.js";
 import viewTab from "./view_tab/FNAbviewtab.js";
 
-const AllPlugins = [viewTab, viewList];
+const AllPlugins = [viewTab, viewList, viewImage];
 
 export default {
    load: (AB) => {
