0110cade0fadb494a865b4beb11a3dcacd8eda88
Wirachot <wirachot@gmail.com>
2026-02-17 19:48:16 +0700

    clean code

--- AppBuilder/platform/plugins/included/view_image/FNAbviewimage.js
+++ AppBuilder/platform/plugins/included/view_image/FNAbviewimage.js
@@ -1,6 +1,5 @@
 import FNAbviewimageComponent from "./FNAbviewimageComponent.js";
 
-
 // FNAbviewimage Web
 // A web side import for an ABView.
 //
@@ -8,81 +7,80 @@ export default function FNAbviewimage({
    /*AB,*/
    ABViewWidgetPlugin,
    ABViewComponentPlugin,
-   ABViewContainer
+   ABViewContainer,
 }) {
-   const ABAbviewimageComponent = FNAbviewimageComponent({ ABViewComponentPlugin });
+   const ABAbviewimageComponent = FNAbviewimageComponent({
+      ABViewComponentPlugin,
+   });
 
-const ABViewImagePropertyComponentDefaults = {
-   filename: "",
-   width: 200,
-   height: 100,
-};
+   const ABViewImagePropertyComponentDefaults = {
+      filename: "",
+      width: 200,
+      height: 100,
+   };
 
-const ABViewDefaults = {
-   key: "image", // {string} unique key for this view
-   icon: "picture-o", // {string} fa-[icon] reference for this view
-   labelKey: "Image", // {string} the multilingual label key for the class label
-};
+   const ABViewDefaults = {
+      key: "image", // {string} unique key for this view
+      icon: "picture-o", // {string} fa-[icon] reference for this view
+      labelKey: "Image", // {string} the multilingual label key for the class label
+   };
 
-class ABViewImageCore extends ABViewWidgetPlugin {
-   constructor(values, application, parent, defaultValues) {
-      super(values, application, parent, defaultValues || ABViewDefaults);
+   class ABViewImageCore extends ABViewWidgetPlugin {
+      constructor(values, application, parent, defaultValues) {
+         super(values, application, parent, defaultValues || ABViewDefaults);
+      }
+
+      static common() {
+         return ABViewDefaults;
+      }
+
+      static defaultValues() {
+         return ABViewImagePropertyComponentDefaults;
+      }
+
+      ///
+      /// Instance Methods
+      ///
+
+      /**
+       * @method componentList
+       * return the list of components available on this view to display in the editor.
+       */
+      componentList() {
+         return [];
+      }
+
+      /**
+       * @property datacollection
+       * return data source
+       * NOTE: this view doesn't track a DataCollection.
+       * @return {ABDataCollection}
+       */
+      get datacollection() {
+         return null;
+      }
+
+      /**
+       * @method fromValues()
+       *
+       * initialze this object with the given set of values.
+       * @param {obj} values
+       */
+      fromValues(values) {
+         super.fromValues(values);
+
+         // convert from "0" => 0
+         this.settings.width = parseInt(
+            this.settings.width || ABViewImagePropertyComponentDefaults.width
+         );
+         this.settings.height = parseInt(
+            this.settings.height || ABViewImagePropertyComponentDefaults.height
+         );
+      }
    }
 
-   static common() {
-      return ABViewDefaults;
-   }
-
-   static defaultValues() {
-      return ABViewImagePropertyComponentDefaults;
-   }
-
-   ///
-   /// Instance Methods
-   ///
-
-   /**
-    * @method componentList
-    * return the list of components available on this view to display in the editor.
-    */
-   componentList() {
-      return [];
-   }
-
-   /**
-    * @property datacollection
-    * return data source
-    * NOTE: this view doesn't track a DataCollection.
-    * @return {ABDataCollection}
-    */
-   get datacollection() {
-      return null;
-   }
-
-   /**
-    * @method fromValues()
-    *
-    * initialze this object with the given set of values.
-    * @param {obj} values
-    */
-   fromValues(values) {
-      super.fromValues(values);
-
-      // convert from "0" => 0
-      this.settings.width = parseInt(
-         this.settings.width || ABViewImagePropertyComponentDefaults.width
-      );
-      this.settings.height = parseInt(
-         this.settings.height || ABViewImagePropertyComponentDefaults.height
-      );
-   }
-};
-
-
-
-return class ABViewImage extends ABViewImageCore {
-
-/**
+   return class ABViewImage extends ABViewImageCore {
+      /**
        * @method getPluginKey
        * return the plugin key for this view.
        * @return {string} plugin key
@@ -91,7 +89,7 @@ return class ABViewImage extends ABViewImageCore {
          return this.common().key;
       }
 
-/**
+      /**
        * @method component()
        * return a UI component based upon this view.
        * @return {obj} UI component
@@ -100,25 +98,20 @@ return class ABViewImage extends ABViewImageCore {
          return new ABAbviewimageComponent(this, parentId);
       }
 
+      // constructor(values, application, parent, defaultValues) {
+      //    super(values, application, parent, defaultValues);
+      // }
 
-   // constructor(values, application, parent, defaultValues) {
-   //    super(values, application, parent, defaultValues);
-   // }
+      //
+      //	Editor Related
+      //
 
-   //
-   //	Editor Related
-   //
+      warningsEval() {
+         super.warningsEval();
 
-   
-
-   warningsEval() {
-      super.warningsEval();
-
-      if (!this.settings.filename) {
-         this.warningsMessage(`has no image set`);
+         if (!this.settings.filename) {
+            this.warningsMessage(`has no image set`);
+         }
       }
-   }
-};
-
+   };
 }
-
