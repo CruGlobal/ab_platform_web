d2c104549bdd74b9f747ea620c7ad8d103dc4286
joshua_cm <bylucygrace@gmail.com>
2026-02-23 22:04:07 +0700

    feat: migrate ABViewForm to plugin architecture

--- AppBuilder/platform/plugins/included/index.js
+++ AppBuilder/platform/plugins/included/index.js
@@ -1,8 +1,10 @@
+import viewForm from "./view_form/FNAbviewform.js";
 import viewList from "./view_list/FNAbviewlist.js";
 import viewTab from "./view_tab/FNAbviewtab.js";
 import viewDetail from "./view_detail/FNAbviewdetail.js";
+import viewMenu from "./view_menu/FNAbviewmenu.js";
 
-const AllPlugins = [viewTab, viewList, viewDetail];
+const AllPlugins = [viewTab, viewList, viewDetail, viewMenu, viewForm];
 
 export default {
    load: (AB) => {
