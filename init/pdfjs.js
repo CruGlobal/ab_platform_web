c452b16a2f63a1eee87624ab84e421c252f2234a
BenWirachot <ben.thongtiang@cru.org>
2026-03-19 11:55:15 +0700

    Remove unused pdfjs

--- init/pdfjs.js
+++
@@ -1,9 +0,0 @@
-/**
- * pdfJS - load the pdfjs dependcies including the web worker. This is a
- * seperate file so we dynamically import it where needed, reducing the inital
- * size of the app.
- */
-
-import * as pdfjs from "pdfjs-dist/webpack.mjs";
-
-export default pdfjs;
