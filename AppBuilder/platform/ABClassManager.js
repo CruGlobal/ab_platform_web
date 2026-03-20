1ba5a992cbababd6c8a4c1c233e842c71b566161
roguisharcanetrickster <roguisharcanetrickster@gmail.com>
2026-03-20 16:11:39 +0700

    fix import

--- AppBuilder/platform/ABClassManager.js
+++ AppBuilder/platform/ABClassManager.js
@@ -17,6 +17,7 @@ import ABViewPropertyFilterData from "./views/viewProperties/ABViewPropertyFilte
 import ABViewPropertyLinkPage from "./views/viewProperties/ABViewPropertyLinkPage";
 import ABViewRuleListFormRecordRules from "../rules/ABViewRuleListFormRecordRules";
 import ABViewRuleListFormSubmitRules from "../rules/ABViewRuleListFormSubmitRules";
+import ABViewRuleListFormRecordRules from "../rules/ABViewRuleListFormRecordRules";
 
 // MIGRATION: ABViewManager is depreciated.  Use ABClassManager instead.
 import ABViewManager from "./ABViewManager.js";
@@ -74,6 +75,7 @@ export function getPluginAPI() {
       ABViewPropertyLinkPage,
       ABViewRuleListFormRecordRules,
       ABViewRuleListFormSubmitRules,
+      ABViewRuleListFormRecordRules,
       //  ABFieldPlugin,
       //  ABViewPlugin,
    };
