5a38b58bb20230d293f0b4d4b338f7cd7600454f
roguisharcanetrickster <roguisharcanetrickster@gmail.com>
2026-03-25 10:07:41 +0700

    sort alphabetically

--- AppBuilder/platform/plugins/included/index.js
+++ AppBuilder/platform/plugins/included/index.js
@@ -1,34 +1,32 @@
-import viewDataSelect from "./view_data-select/FNAbviewdataselect.js";
-import viewDetail from "./view_detail/FNAbviewdetail.js";
 import viewCarousel from "./view_carousel/FNAbviewcarousel.js";
 import viewComment from "./view_comment/FNAbviewcomment.js";
-import viewCsvImporter from "./view_csvImporter/FNAbviewcsvimporter.js";
 import viewCsvExporter from "./view_csvExporter/FNAbviewcsvexporter.js";
-import viewList from "./view_list/FNAbviewlist.js";
-import viewTab from "./view_tab/FNAbviewtab.js";
+import viewCsvImporter from "./view_csvImporter/FNAbviewcsvimporter.js";
+import viewDataSelect from "./view_data-select/FNAbviewdataselect.js";
 import viewDetail from "./view_detail/FNAbviewdetail.js";
-import viewLabel from "./view_label/FNAbviewlabel.js";
-import viewText from "./view_text/FNAbviewtext.js";
 import viewImage from "./view_image/FNAbviewimage.js";
-import viewPdfImporter from "./view_pdfImporter/FNAbviewpdfimporter.js";
+import viewLabel from "./view_label/FNAbviewlabel.js";
 import viewLayout from "./view_layout/FNAbviewlayout.js";
+import viewList from "./view_list/FNAbviewlist.js";
+import viewPdfImporter from "./view_pdfImporter/FNAbviewpdfimporter.js";
+import viewTab from "./view_tab/FNAbviewtab.js";
+import viewText from "./view_text/FNAbviewtext.js";
 
 const AllPlugins = [
-   viewDetail,
+   viewCarousel,
+   viewComment,
    viewCsvExporter,
    viewCsvImporter,
-   viewTab,
-   viewList,
-   viewDetail,
-   viewText,
-   viewLabel,
-   viewImage,
-   viewDataSelect,
-   viewPdfImporter,
-   viewCarousel,
-   viewLayout,
    viewCsvImporter,
-   viewComment,
+   viewDataSelect,
+   viewDetail,
+   viewImage,
+   viewLabel,
+   viewLayout,
+   viewList,
+   viewPdfImporter,
+   viewTab,
+   viewText,
 ];
 
 export default {
