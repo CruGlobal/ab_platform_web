12f90621eec8dd976d78f4b37d43414f9bc191d1
BenWirachot <ben.thongtiang@cru.org>
2026-06-17 16:13:20 +0700

    Migrate tree component to forminput

--- AppBuilder/platform/plugins/included/view_form/viewComponent/FNAbviewformTreeComponent.js
+++ AppBuilder/platform/plugins/included/view_form/viewComponent/FNAbviewformTreeComponent.js
@@ -36,44 +36,49 @@ export default function FNAbviewformTreeComponent({ ABViewFormItemComponent }) {
 
          if (baseView.settings && baseView.settings.formView) newWidth += 40;
 
-         _ui.view = "template";
-         _ui.css = "webix_el_box";
-         _ui.height =
-            field.settings.useHeight === 1
-               ? parseInt(field.settings.imageHeight)
-               : 38;
-         _ui.borderless = true;
+         _ui.view = "forminput";
+         _ui.labelWidth = 0;
+         _ui.paddingY = 0;
+         _ui.paddingX = 0;
+         _ui.css = "ab-custom-field";
+         _ui.body = {
+            view: "focusabletemplate",
+            css: "customFieldCls",
+            borderless: true,
+            template: (obj) => {
+               let val = self._value || "";
 
-         _ui.template = (obj) => {
-            let val = self._value || "";
-
-            if (typeof val == "string" && val.indexOf("[") === 0) {
-               try {
-                  val = JSON.parse(val);
-               } catch (e) {
-                  /* ignore */
+               if (typeof val == "string" && val.indexOf("[") === 0) {
+                  try {
+                     val = JSON.parse(val);
+                  } catch (e) {
+                     /* ignore */
+                  }
                }
-            }
 
-            const rowData = { [field.columnName]: val };
-            const template = field
-               .columnHeader({
-                  width: newWidth,
-               })
-               .template(rowData);
+               const rowData = { [field.columnName]: val };
+               const template = field
+                  .columnHeader({
+                     width: newWidth,
+                  })
+                  .template(rowData);
 
-            return `<div class="customField">${templateLabel}${template}</div>`;
-         };
+               return `<div class="customField">${templateLabel}${template}</div>`;
+            },
+            height:
+               field.settings.useHeight === 1
+                  ? parseInt(field.settings.imageHeight)
+                  : 38,
+            onClick: {
+               customField: (id, e, trg) => {
+                  const node = $$(this.ids.formItem).$view;
 
-         _ui.onClick = {
-            customField: (id, e, trg) => {
-               const node = $$(this.ids.formItem).$view;
-
-               field.customEdit(
-                  { [field.columnName]: self.getValue() },
-                  node,
-                  this
-               );
+                  field.customEdit(
+                     { [field.columnName]: self.getValue() },
+                     node,
+                     this
+                  );
+               },
             },
          };
 
@@ -82,24 +87,58 @@ export default function FNAbviewformTreeComponent({ ABViewFormItemComponent }) {
                if (this.config.value) {
                   self._value = this.config.value;
                }
-
-               this.setValue = (vals) => {
-                  self._value = vals;
-                  this.refresh();
-               };
-               this.getValue = () => {
-                  return self._value || "";
-               };
-               this.setValues = (vals) => {
-                  this.setValue(vals);
-               };
-               this.getValues = () => {
-                  return this.getValue();
-               };
             },
          };
 
          return super.ui(_ui);
+      }
+
+      init(AB) {
+         this.AB = AB;
+
+         const $formItem = $$(this.ids.formItem);
+         if ($formItem) {
+            $formItem.setValue = (vals) => {
+               let cleanVals = vals;
+               if (typeof cleanVals === "string") {
+                  if (cleanVals === "") {
+                     cleanVals = "";
+                  } else if (cleanVals.indexOf("[") === 0) {
+                     try {
+                        cleanVals = JSON.parse(cleanVals);
+                      } catch (e) {
+                        cleanVals = cleanVals.split(",").filter((v) => v !== "");
+                     }
+                  } else {
+                     cleanVals = cleanVals.split(",").filter((v) => v !== "");
+                  }
+               }
+               
+               if (cleanVals == null) {
+                  cleanVals = "";
+               } else if (cleanVals !== "" && !Array.isArray(cleanVals)) {
+                  cleanVals = [cleanVals];
+               }
+
+               this._value = cleanVals;
+               if (typeof $formItem.refresh === "function") {
+                  $formItem.refresh();
+               } else if (typeof $formItem.getBody === "function" && typeof $formItem.getBody().refresh === "function") {
+                  $formItem.getBody().refresh();
+               }
+            };
+            $formItem.getValue = () => {
+               return this._value || "";
+            };
+            $formItem.setValues = (vals) => {
+               $formItem.setValue(vals);
+            };
+            $formItem.getValues = () => {
+               return $formItem.getValue();
+            };
+         }
+
+         return super.init(AB);
       }
 
       getValue(rowData) {
@@ -111,7 +150,7 @@ export default function FNAbviewformTreeComponent({ ABViewFormItemComponent }) {
          // Pass empty string if the returned values is empty array
          if (Array.isArray(vals) && vals.length === 0) vals = "";
 
-         return vals;
+         return vals || "";
       }
    };
 }
